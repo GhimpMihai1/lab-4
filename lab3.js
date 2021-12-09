0918b6e1d8898ba4774b1010294dc7998fc2e389
GhimpMihai1 <94912446+GhimpMihai1@users.noreply.github.com>
2021-12-09 13:21:07 +0200

    Update lab3.js

--- lab3.js
+++ lab3.js
@@ -24,10 +24,46 @@ function toggle() {
   }
   function display(element){
     document.getElementById('image').innerHTML = element.alt;
-    document.getElementById('side').innerHTML = element.alt;
   }
   function revert(){
     document.getElementById('image').innerHTML = "Personajul principal al povestii.";
-    document.getElementById('side').innerHTML ='Afla mai multe despre Ion Creanga.';
   }
- 
+ const navToggle=document.querySelector(".nav-toggle");
+ const links=document.querySelector(".links");
+
+ navToggle.addEventListener('click', function()){
+   if(links.classList.contains('show-links')){
+     links.classList.remove('show-links')
+   }
+   else{
+     links.classList.add('show-links');
+   }
+ });
+ var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
